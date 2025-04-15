b5388bd0632ce00d2b49884d1c16c8b403a5969b
Scryptjo <scryptjo@icloud.com>
2025-04-15 18:06:19 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,20 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('poke-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    event.waitUntil(
+      caches.open('pkm-cache-v1').then(cache => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './style.css',
+          './script.js',
+          './manifest.json'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  });
+  
